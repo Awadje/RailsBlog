4e21edad6b529779ad208f7f01de8c5ead7d50da
Awadje <michaelawad85@gmail.com>
2017-06-28 12:43:43 +0200

    Add edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,11 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
-    @articles = Article.new
+    @article = Article.new
   end
 
   def edit
