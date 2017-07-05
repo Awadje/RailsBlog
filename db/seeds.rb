7d2e760583c80e441bcb68460cff7f7810d7df8f
Awadje <michaelawad85@gmail.com>
2017-07-05 20:33:22 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
-2.times { Article.create!(title: "Title", description: "I'm a description") }
+5.times { Article.create!(title: "Title", description: "I'm a description") }
+1.time { User.create!(username: "Awad", email: "michaelawad85@gmail.com") }
