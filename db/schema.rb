30045299c2d2650f2ccb9a32d775942c6076f7c7
Awadje <michaelawad85@gmail.com>
2017-07-05 22:04:13 +0200

    Add Admin Role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704182203) do
+ActiveRecord::Schema.define(version: 20170705195107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170704182203) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
