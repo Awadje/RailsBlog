94422e68d3f8f695f5204357a1f4e6971e74df2b
Awadje <michaelawad85@gmail.com>
2017-07-05 14:46:12 +0200

    Create login restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:edit, :update, :show]
 
   def index
     @users = User.all
@@ -19,11 +20,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
       if @user.update(user_params)
         flash[:success] = "Your account was succcesfully updated"
         redirect_to articles_path
@@ -33,12 +32,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
