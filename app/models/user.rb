bba1fa05b844a3170949d60b18bf58901c74649e
Awadje <michaelawad85@gmail.com>
2017-07-05 20:36:05 +0200

    Changes for deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,3 @@
 class User < ActiveRecord::Base
-  has_many :articles
-  before_save { self.email = email.downcase }
-  validates :username, uniqueness: true,
-            presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, uniqueness: true,
-            presence: true,
-            format: { with: VALID_EMAIL_REGEX }
-  has_secure_password
+
 end
