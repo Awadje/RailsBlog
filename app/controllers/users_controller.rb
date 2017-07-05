dc30f9db8b73b7813bc0f9e28b11dd3b6eb5974b
Awadje <michaelawad85@gmail.com>
2017-07-05 20:09:15 +0200

    minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only:[:edit, :update, :show]
-  before_action :require_user
+  before_action :require_user, except:[:new, :create]
 
   def index
     @users = User.all
